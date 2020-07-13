3e95ab491a59f3bf80c3f42ba11cce5927a55d23
Kislatron <kislatron@gmail.com>
2020-07-13 16:56:34 -0500

    Создано второе активити и переход между ними.

--- app/src/main/java/com/example/studycycles/MainActivity.kt
+++ app/src/main/java/com/example/studycycles/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.studycycles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn.setOnClickListener{
+            val intent = Intent(this, Timer_Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
